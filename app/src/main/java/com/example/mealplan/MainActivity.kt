bc28bf70f3db5eaeb312173139a28ae6a74a14d8
kimatzu <mattzkie07@gmail.com>
2023-05-21 19:12:40 +0800

    TestMessage

--- app/src/main/java/com/example/mealplan/MainActivity.kt
+++ app/src/main/java/com/example/mealplan/MainActivity.kt
@@ -11,7 +11,7 @@ class  MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     //test comment
-
+        //testttt
 
         val buttonClick = findViewById<Button>(R.id.PressToContinue)
         buttonClick.setOnClickListener {
