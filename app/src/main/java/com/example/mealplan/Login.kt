a7429653d369791c757419ead608adee0068e957
kimatzu <mattzkie07@gmail.com>
2023-05-21 20:19:33 +0800

    Updated color theme

--- app/src/main/java/com/example/mealplan/Login.kt
+++ app/src/main/java/com/example/mealplan/Login.kt
@@ -29,6 +29,7 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
 
 
         // Check if user is already logged in
